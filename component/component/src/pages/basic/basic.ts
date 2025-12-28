f6a9095d022e2c70625329a06c9d9920880e9c09
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-12-28 07:31:08 +0530

    Basic_File

--- component/component/src/pages/basic/basic.ts
+++ component/component/src/pages/basic/basic.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { provideRoutes, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-basic',
   imports: [],
-  templateUrl: './basic.html',
+
+//   template:`<h1>This is Child Component</h1>
+// <h3>This is My Information</h3>
+// <p>Name : {{name}}</p>
+// <p>Mark: {{marks}}</p>
+// <button (click)="showMessage()">Click Me</button>`,
+
+   templateUrl: './basic.html',
   styleUrl: './basic.css',
+  // styles : [`
+  //   h1{
+  //     color : blue;
+  //     font-weight: bold;
+  //   }`]
+  standalone : true,
+  // providers:[DatePipe]
 })
 export class Basic {
+name : string = "Prachi Jadhav";
+marks : number=98;
+// liveDate :any;
 
+
+showMessage(){
+  alert("In Basic Component Button Clicked");
+}
+// show(){
+//   this.liveDate = this.date.trasforms(new Date(),'dd-MM-yyy');
+// }
 }
