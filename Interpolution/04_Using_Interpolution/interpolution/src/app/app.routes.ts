405f8150cdf9e6cebd58184994b31aa9d734c030
PrachiJadhav14 <prachijadhav5514@gmail.com>
2026-04-22 16:31:33 +0530

    Interpolutin

--- Interpolution/04_Using_Interpolution/interpolution/src/app/app.routes.ts
+++ Interpolution/04_Using_Interpolution/interpolution/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
+import { Lifecycle } from './lifecycle/lifecycle';
+import { Prachi } from './prachi/prachi';
+import { Student } from './student/student';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {path :'',component: App},
+  {path :'lifecycle',component :Lifecycle},
+   {path :'prachi',component: Prachi},
+   {path : 'student', component: Student}
+
+];
