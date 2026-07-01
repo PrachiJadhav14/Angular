f6198ba4432bddb28bb884874df69db005aa99e7
PrachiJadhav14 <prachijadhav5514@gmail.com>
2026-07-01 17:03:47 +0530

    Directives

---
+++ Directives/01_Directives/Demo/src/app/attribute/attribute.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-attribute',
+  imports: [FormsModule, CommonModule],
+  templateUrl: './attribute.html',
+  styleUrl: './attribute.css',
+})
+export class Attribute {
+ username ="";
+ selected = 'home'
+ isError : boolean = true ;
+//  isError : boolean = false ;
+}
