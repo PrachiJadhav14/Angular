d0005b146f66bcdc8128faa92a9c5ec63e1b3e18
PrachiJadhav14 <prachijadhav5514@gmail.com>
2026-07-06 16:29:03 +0530

    Directive

---
+++ Directives/01_Directives/Demo/src/highlight-demo.spec.ts
@@ -0,0 +1,8 @@
+import { HighlightDemo } from './highlight-demo';
+
+describe('HighlightDemo', () => {
+  it('should create an instance', () => {
+    const directive = new HighlightDemo();
+    expect(directive).toBeTruthy();
+  });
+});
