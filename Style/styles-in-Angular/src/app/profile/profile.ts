21358bf067e21f646b36cb543a8d6b2cbff5cd7e
PrachiJadhav14 <prachijadhav5514@gmail.com>
2026-07-14 21:47:23 +0530

    Style

---
+++ Style/styles-in-Angular/src/app/profile/profile.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  imports: [],
+  templateUrl: './profile.html',
+  styleUrls: ['./profile.btn.css','./profile.css']
+})
+export class Profile {}
