40d5693461a7b30a883ca631f13bbf9885312a9d
PrachiJadhav14 <prachijadhav5514@gmail.com>
2026-08-18 15:48:35 +0530

    Component

--- component/component/src/app/app.ts
+++ component/component/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Basic } from "../pages/basic/basic";
+import { MainLayout } from "./main-layout/main-layout";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Basic],
+  imports: [MainLayout],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
